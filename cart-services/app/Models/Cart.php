519dd7cdfaf74a402921a2cee1f84e4e2da96358
Herurafki <herurafki11@gmail.com>
2024-12-29 08:07:55 +0700

    first commit

---
+++ cart-services/app/Models/Cart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model{
+    protected $table = 'cart_items';
+
+    protected $fillable = [
+        'product_id',
+        'name',
+        'quantity',
+        'price',
+    ];
+}
+
+?>
